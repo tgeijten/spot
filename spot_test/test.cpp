0815133be6ea3a8d99547086a2ec343dd5f9f24d
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-09-10 13:46:58 +0200

    U: spot test now uses XO_TEST_MAIN

--- spot_test/test.cpp
+++ spot_test/test.cpp
@@ -1,29 +1,3 @@
-#include "test.h"
-#include "xo/system/log_sink.h"
-#include "xo/system/test_case.h"
-#include "test_functions.h"
-#include "optimization_test.h"
-#include <iostream>
+#include "xo/system/test_main.h"
 
-using xo::string;
-
-int main( int argc, char* argv[] )
-{
-	xo::log::stream_sink str( xo::log::info_level, std::cout );
-	xo::log::add_sink( &str );
-
-	try
-	{
-		spot::multi_optimizer_test();
-	}
-	catch ( std::exception& e )
-	{
-		std::cout << e.what();				
-	}
-
-#ifdef _DEBUG
-	xo::wait_for_key();
-#endif
-
-	return 0;
-}
+XO_TEST_MAIN
