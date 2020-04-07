bf0a0804589c9afa4141425a02f43ca8d224906a
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-04-07 22:10:59 +0200

    U: evaluator_test

--- spot_test/evaluator_test.cpp
+++ spot_test/evaluator_test.cpp
@@ -1,24 +1,23 @@
 #include "xo/system/test_case.h"
 
-#include "test_functions.h"
 #include "spot/cma_optimizer.h"
-#include "spot/pooled_evaluator.h"
+#include "test_functions.h"
 #include "xo/system/log.h"
-#include "spot/async_evaluator.h"
 #include "xo/time/stopwatch.h"
-#include <thread>
+#include "spot/async_evaluator.h"
+#include "spot/batch_evaluator.h"
+#include "spot/pooled_evaluator.h"
 #include <chrono>
-#include <iostream>
+#include <thread>
 
 using namespace std::chrono_literals;
-using std::cout;
 
 namespace spot
 {
-	inline constexpr int g_dim = 16;
-	inline constexpr cma_options g_cma{ 0 };
-	inline constexpr int g_iterations = 10;
-	inline constexpr int g_optimizers = 10;
+	inline constexpr int g_dim = 12;
+	inline constexpr cma_options g_cma{ 32 };
+	inline constexpr int g_iterations = 100;
+	inline constexpr int g_optimizers = 20;
 
 	fitness_t evaluator_test( evaluator& e, size_t num_opt = g_optimizers )
 	{
@@ -33,9 +32,10 @@ namespace spot
 
 		for ( auto& opt : optimizers )
 		{
-			cout << "starting " << threads.size() << " lambda=" << opt->lambda() << "\n";
+			printf( "+%zd", threads.size() );
 			threads.push_back( std::async( std::launch::async, [&]( cma_optimizer& o ) { return o.run(); }, std::ref( *opt ) ) );
 		}
+		printf( " " );
 
 		size_t finished = 0;
 		while ( finished < threads.size() )
@@ -43,12 +43,12 @@ namespace spot
 			for ( index_t i = 0; i < threads.size(); ++i )
 				if ( threads[ i ].valid() && threads[ i ].wait_for( 1ms ) == std::future_status::ready )
 				{
-					cout << "finished " << i << "\n";
+					printf( "-%zd", i );
 					threads[ i ] = std::future<const stop_condition*>();
 					++finished;
 				}
 		}
-		cout << "DONE!" << std::endl;
+		printf( "...DONE!\n" );
 
 		return optimizers.front()->best_fitness();
 	}
@@ -56,19 +56,21 @@ namespace spot
 	XO_TEST_CASE( evaluator_test )
 	{
 		auto seq_eval = sequential_evaluator();
-		auto async_eval = async_evaluator( 64, xo::thread_priority::low );
+		auto async_eval = async_evaluator( 0, xo::thread_priority::low );
+		auto batch_eval = batch_evaluator( xo::thread_priority::low );
 		auto pooled_eval = pooled_evaluator( 0, xo::thread_priority::low );
 
 		xo::stopwatch sw;
-		auto f1 = evaluator_test( seq_eval );
+		auto fs = evaluator_test( seq_eval );
 		sw.add_measure( "seq" );
-		auto f2 = evaluator_test( async_eval );
+		auto fa = evaluator_test( async_eval );
 		sw.add_measure( "async" );
-		auto f3 = evaluator_test( pooled_eval );
+		auto fb = evaluator_test( batch_eval );
+		sw.add_measure( "batch" );
+		auto fp = evaluator_test( pooled_eval );
 		sw.add_measure( "pool" );
 
-		//XO_CHECK( f1 == f2 );
-		XO_CHECK( f2 == f3 );
+		XO_CHECK( fa == fp );
 
 		xo::log::info( "results:\n", sw.get_report() );
 	}
