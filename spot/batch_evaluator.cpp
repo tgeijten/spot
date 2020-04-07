5fffe2a52460bb9ba7e79b499886aad0e64e98a3
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-04-07 21:55:56 +0200

    N: batch_evaluator

---
+++ spot/batch_evaluator.cpp
@@ -0,0 +1,39 @@
+#include "batch_evaluator.h"
+
+#include "objective.h"
+
+namespace spot
+{
+	batch_evaluator::batch_evaluator( xo::thread_priority thread_prio ) :
+		evaluator(),
+		thread_prio_( thread_prio )
+	{}
+
+	vector< result<fitness_t> > batch_evaluator::evaluate( const objective& o, const search_point_vec& point_vec, priority_t prio )
+	{
+		// create threads
+		vector< std::future< xo::result< fitness_t > > > futures;
+		for ( const auto& point : point_vec )
+		{
+			futures.emplace_back(
+				std::async( std::launch::async,	[&]() {
+					xo::set_thread_priority( thread_prio_ );
+					return o.evaluate_noexcept( point );
+				} )
+			);
+		}
+
+		// wait for results
+		vector< result<fitness_t> > results;
+		results.reserve( point_vec.size() );
+		for ( auto& f : futures )
+			results.push_back( f.get() );
+
+		return results;
+	}
+
+	void batch_evaluator::set_thread_priority( xo::thread_priority prio )
+	{
+		thread_prio_ = prio;
+	}
+}
---
+++ spot/batch_evaluator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "spot/spot_types.h"
+#include "spot/search_point.h"
+#include "evaluator.h"
+#include <future>
+
+namespace spot
+{
+	class SPOT_API batch_evaluator : public evaluator
+	{
+	public:
+		batch_evaluator( xo::thread_priority thread_prio = xo::thread_priority::low );
+		virtual vector<result<fitness_t>> evaluate( const objective& o, const search_point_vec& point_vec, priority_t prio = 0 ) override;
+		void set_thread_priority( xo::thread_priority prio );
+
+	protected:
+		xo::thread_priority thread_prio_;
+	};
+}
